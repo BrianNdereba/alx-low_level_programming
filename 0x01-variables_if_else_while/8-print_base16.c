4f113d48d8e7e3533098d8c044782078275fd03e
Brian Ndereba <brianndereba7@gmail.com>
2023-03-17 12:43:54 -0700

    Print hexadecimals

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -8,10 +8,15 @@
 
 int main(void)
 {
+	int i;
 	char c;
 
-	for (c = 'z'; c >= 'a'; c--)
+	for (i = '0'; i <= '9'; i++)
+	putchar(i);
+
+	for (c = 'a'; c <= 'f'; c++)
 	putchar(c);
 	putchar('\n');
+
 	return (0);
 }
