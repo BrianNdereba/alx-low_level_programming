341bd6592e76c653eb5be4163449257d0d35436f
Brian Ndereba <brianndereba7@gmail.com>
2023-03-21 15:20:34 -0700

    Natural numbers

---
+++ 0x02-functions_nested_loops/101_natural.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int x, y = 0;
+
+	while (x < 1024)
+	{
+	if ((x % 3 == 0) || (x % 5 == 0))
+	{
+	y += x;
+	}
+	x++;
+	}
+	printf("%d", y);
+	return (0);
+}
