1073f8670082ef86a8b5e8f5d4a3b0d6dbba5936
Brian Ndereba <brianndereba7@gmail.com>
2023-03-22 16:44:02 -0700

    Fibonacci

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -6,3 +6,29 @@
  * Return: Always 0 (Success)
  */
 
+int main(void)
+{
+	int x = 0;
+	long y = 1, z = 2;
+
+	while (x < 50)
+	{
+	if (x == 0)
+	{
+	printf("%d", y);
+	}
+	else if (x == 1)
+	{
+	printf(", %d", z);
+	}
+	else
+	{
+	z += y;
+	y = z - y;
+	printf(", %d", z);
+	}
+	++x;
+	}
+	printf("\n");
+	return (0);
+}
