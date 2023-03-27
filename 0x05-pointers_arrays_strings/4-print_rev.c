6868cadd8b6c1f16999a6ae6528ecb7966062bc4
Brian Ndereba <brianndereba7@gmail.com>
2023-03-27 11:09:11 -0700

    print in reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,23 +8,19 @@
 
 void print_rev(char *s)
 {
-	int _strlen(char *s)
-	{
-		int len = 0;
-
-		while (*s != '\0')
-		{
-			len++;
-			s++;
-		}
-		return (len);
-	}
-	int lenx = _strlen(s);
 	int x;
+	int len = 0;
 
-	while ((x = (lenx - 1) >= 0))
+	while (*s != '\0')
 	{
-		_putchar(*(s + x));
+		len++;
+		s++;
+	}
+	s--;
+	for (x = len; x > 0; x--)
+	{
+		_putchar(*s);
+		s--;
 	}
 	_putchar('\n');
 }
