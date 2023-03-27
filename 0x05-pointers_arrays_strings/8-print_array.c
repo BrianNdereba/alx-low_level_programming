e0bc1055f32ace691b92b075e968500efdd98de2
Brian Ndereba <brianndereba7@gmail.com>
2023-03-27 13:55:09 -0700

    Array Elements

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_array - print elements in array
+ * @a: Array
+ * @n: elemets in array
+ * Return: Nothing
+ */
+
+void print_array(int *a, int n)
+{
+	int x;
+
+	for (x = 0; a[i] < a[n]; x++)
+	{
+		if (x != (n - 1))
+		{
+			printf("%d, ", a[x]);
+		}
+		else
+		{
+			printf("%d", a[x]);
+		}
+	}
+	printf("\n");
+}
+
