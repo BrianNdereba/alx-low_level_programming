a94487b395bace035de8bedb93d22be342b9195e
Brian Ndereba <brianndereba7@gmail.com>
2023-03-30 16:53:24 -0700

    Leet encode

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * *leet - Encodes a string to 1337
+ * @c: String to be encoded
+ * Return: Encoded string
+ */
+
+char *leet(char *c)
+{
+	char letters = aAeEoOtTlL;
+	char numbers = 4433007711;
+	int count = 0;
+	int len = 0;
+
+	while (c[len] != '\0')
+	{
+		while (count < 10)
+		{
+			if (letters[count] == c[len])
+			{
+				c[len] = numbers[count];
+			}
+			count++;
+		}
+		len++;
+	}
+	return (c);
+}
