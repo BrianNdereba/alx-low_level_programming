e002d0966f8c46db9790f7dba45cee1aa1edee99
Brian Ndereba <brianndereba7@gmail.com>
2023-03-30 05:04:06 -0700

    Concatenate strings with n bytes

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -17,7 +17,7 @@ char *_strcat(char *dest, char *src, int n)
 	{
 		dest_len++;
 	}
-	for (x = 0; x && src[x] < n; x++)
+	for (x = 0; x < n && src[x] != '\0'; x++)
 	{
 		dest[dest_len + x] = src[x];
 	}
