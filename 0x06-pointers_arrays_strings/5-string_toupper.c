de134bcc900e58acbb4e7ed20b9bdb9d5dc93767
Brian Ndereba <brianndereba7@gmail.com>
2023-03-30 14:06:11 -0700

    Lower to Upper

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * *string_toupper - Changes lowercase letters to uppercase
+ * @x: String to be changed
+ * Return: Changed string
+ */
+
+char *string_toupper(char *x)
+{
+	int len = 0;
+
+	while (x[len] != '\0')
+	{
+		for (x[len] >= 97 && x[len] <= 122)
+		{
+			x[len] = x[len] - 32;
+		}
+		len++;
+	}
+	return (x);
+}
