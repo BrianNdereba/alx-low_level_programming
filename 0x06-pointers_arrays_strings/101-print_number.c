9414ce5cb999948cacc1e120a478d551884d2da7
Brian Ndereba <brianndereba7@gmail.com>
2023-03-30 18:00:54 -0700

    Print integer

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_number - Prints an integer
+ * @n: Integer to be printed
+ * Return: Nothing
+ */
+
+void print_number(int n)
+{
+	unsigned int x;
+
+	x = n;
+	if (n < 0)
+	{
+		n = -n;
+		_putchar('-');
+	}
+	if ((x / 10) != 0)
+	{
+		print_number(x / 10);
+	}
+	_putchar((x % 10) + '0');
+}
