9d74f85e159d8d3690ad29700558062e8a6417e0
Brian Ndereba <brianndereba7@gmail.com>
2023-04-05 05:17:45 -0700

    Power

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - Returns value of x raised to power y
+ * @x: Base integer
+ * @y: Exponent integer
+ * Return: Value of x raised to power y
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else if (y == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (x * _pow_recursion(x, (y - 1)));
+	}
+}
