3b74e5be2129a62e93a0eeca23313ae913d348fc
Brian Ndereba <brianndereba7@gmail.com>
2023-04-05 03:19:42 -0700

    Puts with recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - Prints a string followed by a new line
+ * @s: String to be printed
+ * Return: Nothing
+ */
+
+void _puts_recursion(char *s)
+{
+	int x;
+
+	x = 0;
+	if (s[x] == '\0')
+	{
+		_putchar('\n');
+	}
+	else 
+	{
+		_putchar(s[x]);
+		_puts_recursion(s + 1);
+	}
+}
